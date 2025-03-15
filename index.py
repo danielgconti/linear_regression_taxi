76d3391da1ac7adad8a8ef57d6894a722f42855d
Daniel Golia Conti <contidan@msu.edu>
2025-03-15 18:12:30 -0400

    Changes

---
+++ index.py
@@ -0,0 +1,18 @@
+#@title Code - Load dependencies
+
+#general
+import io
+
+# data
+import numpy as np
+import pandas as pd 
+
+# machine learning
+from tensorflow import keras
+
+# data visualization
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
+
